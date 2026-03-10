f385c6864726a67f862a3919bca1f599b182f8cf
kelemeno <kl@matterlabs.dev>
2026-03-10 13:00:31 +0000

    less sorrys

--- specs/L1AssetRouter/L1AssetRouter/Common/if_2507078964606319426_user.lean
+++ specs/L1AssetRouter/L1AssetRouter/Common/if_2507078964606319426_user.lean
@@ -11,12 +11,14 @@ section
 
 open Clear EVMState Ast Expr Stmt FunctionDefinition State Interpreter ExecLemmas OutOfFuelLemmas Abstraction YulNotation PrimOps ReasoningPrinciple Utilities L1AssetRouter.Common 
 
-def A_if_2507078964606319426 (s₀ s₉ : State) : Prop := sorry
+def A_if_2507078964606319426 (s₀ s₉ : State) : Prop :=
+  if_2507078964606319426_concrete_of_code.1 s₀ s₉
 
 lemma if_2507078964606319426_abs_of_concrete {s₀ s₉ : State} :
   Spec if_2507078964606319426_concrete_of_code s₀ s₉ →
   Spec A_if_2507078964606319426 s₀ s₉ := by
-  sorry
+  intro h
+  simpa [A_if_2507078964606319426] using h
 
 end
 
--- specs/L1AssetRouter/L1AssetRouter/Common/if_510303534436936689_user.lean
+++ specs/L1AssetRouter/L1AssetRouter/Common/if_510303534436936689_user.lean
@@ -10,12 +10,14 @@ section
 
 open Clear EVMState Ast Expr Stmt FunctionDefinition State Interpreter ExecLemmas OutOfFuelLemmas Abstraction YulNotation PrimOps ReasoningPrinciple Utilities 
 
-def A_if_510303534436936689 (s₀ s₉ : State) : Prop := sorry
+def A_if_510303534436936689 (s₀ s₉ : State) : Prop :=
+  if_510303534436936689_concrete_of_code.1 s₀ s₉
 
 lemma if_510303534436936689_abs_of_concrete {s₀ s₉ : State} :
   Spec if_510303534436936689_concrete_of_code s₀ s₉ →
   Spec A_if_510303534436936689 s₀ s₉ := by
-  sorry
+  intro h
+  simpa [A_if_510303534436936689] using h
 
 end
 
